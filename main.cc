7e6eaf01e44ca58c5d9678f803118b537b3902eb
null <30485571+nullnan@users.noreply.github.com>
2024-06-11 22:00:58 +0800

    袭击不让投洋🍐，删除一下捏

--- main.cc
+++ main.cc
@@ -8,58 +8,6 @@
 int main(int argc, const char** argv) {
 	/* 解析命令行选项 */
 	Flag::getFlag().init(argc, argv);
-
-#ifdef OUTPUT_OJ_INPUT
-	char c;
-	while ((c = fgetc(yyin)) != EOF) {
-		if (c == '\n') continue;
-		printf("%c", c);
-	}
-	printf("\n");
-	rewind(yyin);
-#endif
-#if 0
-	struct ASTNode* root = NULL;
-	int result = yyparse(&root);
-	if (result == 0) {
-		printf("====AST Info====\n");
-		// ASTNode_print(root);
-		QueryResult* result = ASTNode_querySelector(root, "/"), * cur = NULL;
-
-		int count = 0;
-		DL_FOREACH(result, cur) {
-			printf("=== Result %d ===\n", ++count);
-			ASTNode_print(cur->node);
-			printf("=======\n");
-		}
-	}
-#endif
-#if 0
-	Element root = Element::CreateByFile(filename);
-	Query result = root("/");
-	//result[0].table("a").print();
-
-	printf("========\n");
-	for (auto i : root("//Plus")[0]) {
-		printf("<%s%s>\n", i.id(), i.flag ? "/" : "");
-	}
-
-	int count = 0;
-	for (auto cur : result) {
-		printf("=== Result %d ===\n", ++count);
-		cur.print();
-		printf("=======\n");
-	}
-#endif
-#ifdef OUTPUT_OJ_INPUT
-	char c;
-	while ((c = fgetc(yyin)) != EOF) {
-		if (c == '\n') continue;
-		printf("%c", c);
-	}
-	printf("\n");
-	rewind(yyin);
-#endif
   
 	Element root = Element::CreateByFile(Flag::getFlag().getFlagFor("input").c_str());
 	//ConstNode_unfold(root);
