448b2298c0b2e689261825a1d1f835e8b8cd8999
null <30485571+nullnan@users.noreply.github.com>
2024-06-13 22:46:22 +0800

    Revert "等下回滚"
    This reverts commit c4b31fab656514a0be72fd4a4efbb77415253e37.

--- main.cc
+++ main.cc
@@ -19,10 +19,6 @@ int main(int argc, const char** argv) {
 
   auto as_file = fopen(Flag::getFlag().getFlagFor("output").c_str(), "w");
 
-  char s[100] = {0};
-
-  sprintf(s, "Hello, world!");
-
   fprintf(as_file, R"(main:
         str     fp, [sp, #-4]!
         add     fp, sp, #0
