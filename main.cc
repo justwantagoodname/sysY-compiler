83ba77e4b12bb661dced3e8bb90b588f195fdb50
null <30485571+nullnan@users.noreply.github.com>
2024-06-14 08:24:44 +0800

    add code builder

--- main.cc
+++ main.cc
@@ -4,18 +4,27 @@
 #include "ast.h"
 #include "pass.h"
 #include "flag.h"
+#include "codegen/ashelper.hpp"
 
 int main(int argc, const char** argv) {
 	/* 解析命令行选项 */
 	Flag::getFlag().init(argc, argv);
   
-	Element root = Element::CreateByFile(Flag::getFlag().getFlagFor("input").c_str());
+	// Element root = Element::CreateByFile(Flag::getFlag().getFlagFor("input").c_str());
 
-  root.print();
-	ConstNode_unfold(root);
-	ArrayDecl_flatten(root);
-  printf("===After flatten===\n");
-  root.print();
+  // root.print();
+	// ConstNode_unfold(root);
+	// ArrayDecl_flatten(root);
+  // printf("===After flatten===\n");
+  // root.print();
+
+  AssemblyBuilder asfile("output.s");
+  
+  asfile| ".data" 
+        | "test"
+        | "test2"
+        | "test3"
+        | 123;
 
 #ifdef TRIPLE_DEBUG
 	Triples triples(root);
