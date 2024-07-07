f5dda64ce4236b98261ecc6728aebef6a933f457
SamBillon <sambillon1234@gmail.com>
2024-07-07 23:49:39 +0800

    开始RiscV翻译

--- codegen/riscvinstr.cc
+++ codegen/riscvinstr.cc
@@ -1,2 +1,50 @@
 #include "codegen/riscvinstr.h"
 
+RVOperand::RVOperand() : tag(UNDEF), value(0) {
+    return;
+}
+RVOperand::RVOperand(RVOperandTag tag, int value)
+    : tag(tag), value(value) {
+        return;
+}
+
+
+RVOperand make_reg(int reg) {
+    return RVOperand(REG, reg);
+}
+RVOperand make_sreg(int sreg) {
+    return RVOperand(SREG, sreg);
+}
+RVOperand make_imm(int value) {
+    return RVOperand(IMM, value);
+}
+RVOperand make_simm(float value) {
+    return RVOperand(SIMM, value);
+}
+
+RVInstr::RVInstr() : op(NOP) {
+    return;
+}
+RVInstr::RVInstr(RVOp op) : op(op) {
+    return;
+}
+RVBinaryInstr::RVBinaryInstr(RVOperand opr1, RVOperand opr2, RVOperand result)
+    : RVInstr(BINARY), opr1(opr1), opr2(opr2), result(result) {
+    return;
+}
+std::string RVBinaryInstr::toASM() override {
+    std::string result;
+    switch (opt) {
+        case ADD:
+            result += "    add ";
+            break;
+        case SUB:
+            result += "    sub ";
+            break;
+        default:
+            assert(false);
+            break;
+    }
+    result += result.toASM() + ", " + opr1.toASM() + ", " + opr2.toASM();
+    return result;
+}
--- include/codegen/riscvinstr.h
+++ include/codegen/riscvinstr.h
@@ -3,11 +3,56 @@
 #ifndef RISCVINSTR_H_
 #define RISCVINSTR_H_
 
-enum class RiscVOp {
+#include "sysY.h"
 
+enum RVOperandTag {
+    UNDEF,
+    ERROR,
+    REG,
+    SREG,
+    IMM,
+    SIMM,
+    ADDR
 };
-class RiscVInstr {
 
+
+class RVOperand {
+public:
+    RVOperandTag tag;
+    int32_t value;
+    RVOperand();
+    RVOperand(RVOperandTag tag, int value);
+    std::string toASM();
+};
+
+RVOperand make_reg(int reg);
+RVOperand make_sreg(int sreg);
+RVOperand make_imm(int value);
+RVOperand make_simm(float value);
+
+enum RVOp {
+    NOP,
+    BINARY,
+};
+
+class RVInstr {
+public:
+    RVOp op;
+    RVInstr();
+    RVInstr(RVOp op);
+    virtual std::string toASM() = 0;
+};
+
+enum RVBinary {
+    ADD,
+    SUB
+};
+class RVBinaryInstr : public RVInstr {
+public:
+    RVBinary opt;
+    RVOperand opr1, opr2, result;
+    RVBinaryInstr(RVOperand opr1, RVOperand opr2, RVOperand result);
+    virtual std::string toASM() override;
 };
 
 #endif
