1212a734f53323ccf674a4883094617d0ab07024
L-Bromine <lishaofan_31415926@163.com>
2024-08-13 12:50:28 +0800

    切换为三元组arm

--- main.cc
+++ main.cc
@@ -11,7 +11,8 @@
 #define UNI_OPTIMIZTION
 
  //#define ASM_GEN
-#define TRIPLE_DEBUG
+//#define TRIPLE_DEBUG
+#define ARM_TRIPLE_DEBUG
 // #define RV_ASM_GEN
 
 #ifdef TRIPLE_DEBUG
@@ -92,6 +93,43 @@ int main(int argc, const char** argv) {
 	}
 	asm_file.raw(".section	.note.GNU-stack,\"\",%progbits\n.ident	\"SysY-Compiler\"\n");
 #endif
+
+#ifdef ARM_TRIPLE_DEBUG
+	// root.print();
+
+	Triples triples(root);
+	triples.pretreat();
+	printf("===After pretreat===\n");
+
+	root.print();
+
+	printf("===After make===\n");
+	triples.make();
+	root.print();
+	// triples.print();
+
+	printf("===After eliUnnecVar===\n");
+	triples.eliUnnecVar();
+	// triples.print();
+
+	printf("===After MinTemp===\n");
+	triples.minTempVar();
+	// triples.print();
+
+	printf("===After ResortTemp===\n");
+	triples.resortTemp();
+	triples.print();
+
+	panic("DOING……");
+
+	AssemblyBuilder asm_file(Flag::getFlag().by<std::string>("output").c_str());
+	asm_file.raw(".global main\n.text\n.align 2\n.type main, %function\n");
+	//for (auto p : g.instrs) {
+	//	asm_file.raw(p->toASM().c_str());
+	//}
+	asm_file.raw(".section	.note.GNU-stack,\"\",%progbits\n.ident	\"SysY-Compiler\"\n");
+#endif
+
 #ifdef RV_ASM_GEN
 	AssemblyBuilder asm_file(Flag::getFlag().by<std::string>("output").c_str());
 
