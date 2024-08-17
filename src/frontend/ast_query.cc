11fb2d9241882ab5d913c8c31de0a546237e9364
N <example@example.com>
2024-08-17 09:47:25 +0800

    delete vasprintf

--- src/frontend/ast_query.cc
+++ src/frontend/ast_query.cc
@@ -3,24 +3,6 @@
 #include "ast.h"
 #include "query.tab.hh"
 
-int vasprintf(char ** __restrict__ ret,
-                      const char * __restrict__ format,
-                      va_list ap) {
-    int len;
-    /* Get Length */
-    len = _vsnprintf(NULL,0,format,ap);
-    if (len < 0) return -1;
-    /* +1 for \0 terminator. */
-    *ret = static_cast<char *>(malloc(len + 1));
-    /* Check malloc fail*/
-    if (!*ret) return -1;
-    /* Write String */
-    _vsnprintf(*ret,len+1,format,ap);
-    /* Terminate explicitly */
-    (*ret)[len] = '\0';
-    return len;
-}
-
 extern void qqlex_init(yyscan_t *scanner);
 extern void qq_scan_string(const char *str, yyscan_t scanner);
 extern void qqlex_destroy(yyscan_t scanner);
