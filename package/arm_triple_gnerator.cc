878fc3cbcb1c4269fe24fe4bd882490030e02c2f
null <30485571+nullnan@users.noreply.github.com>
2024-08-17 13:45:41 +0800

    大概修正了数组中存储下标寻址立即数的的问题

--- package/arm_triple_gnerator.cc
+++ package/arm_triple_gnerator.cc
@@ -204,7 +204,6 @@ namespace TriplesArmGenerator {
         } else if (addr.base >= AB.r0 && addr.base <= AB.pc || addr.base == AB.reglsl_stack) {
             // 在栈上（以某个寄存器为基偏移）， 读取
             if (stack_type != 2) {
-                assert(addr.value < 1024);
                 instrs.push_back({ ACmd.ldr, reg, addr });
 
             } else {
